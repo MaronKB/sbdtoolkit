4077193cebcd833df3edb6a2a27f2836eb4432dd
MaronKB <asdfgh7913@naver.com>
2021-08-19 14:21:59 +0900

    1.0.2

---
+++ scripts.js
@@ -0,0 +1,31 @@
+class ModuleSettings {
+	static get(key) {
+		return game.settings.get('sbdtoolkit', key)
+	}
+	static register() {
+		game.settings.register('sbdtoolkit', 'quickchat', {
+			name: "빠른 대화 사용",
+			hint: "활성화하면 토큰 선택 즉시 커서가 대화창으로 이동하고, 자동으로 큰따옴표가 입력됩니다.",
+			scope: 'client',
+			config: true,
+			type: Boolean,
+			default: false
+		});
+	}
+}
+Hooks.once('init', async function() {
+	ModuleSettings.register();
+});
+
+Hooks.on('controlToken', GoToChat)
+
+function GoToChat() {
+	if (!ModuleSettings.get('quickchat')) return;
+	const chatmsg = document.getElementById('chat-message');
+	
+	document.querySelector('#sidebar-tabs>a').click();setTimeout(function() { 
+		chatmsg.focus();
+		chatmsg.value = "\"\"";
+		chatmsg.setSelectionRange(1, 1);
+		}, 10);
+}
